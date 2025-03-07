fd0e8865de3cfd4ea805063cf955df93dea82dcb
acitate <m.parsa.kolivand@gmail.com>
2025-03-07 18:35:55 +0330

    Exercise 6.16

---
+++ 12-Exercise6_16/src/Exercise6_16.java
@@ -0,0 +1,10 @@
+public class Exercise6_16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d\t", i * j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
